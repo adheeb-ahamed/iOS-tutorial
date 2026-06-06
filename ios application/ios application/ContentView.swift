160fb76b986518f3fc447142b34b0512e3c4edf4
Student 3 <student3@NIBMADMIN01s-iMac.local>
2026-06-06 16:40:33 +0530

    First commit

---
+++ ios application/ios application/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ios application
+//
+//  Created by Student 3 on 2026-06-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios application/ios application/ios_applicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_applicationApp.swift
+//  ios application
+//
+//  Created by Student 3 on 2026-06-06.
+//
+
+import SwiftUI
+
+@main
+struct ios_applicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
