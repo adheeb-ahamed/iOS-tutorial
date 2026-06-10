4841b6973a3adde0c21f43c0bbc91adf1afd890b
adheeb-ahamed <official.adheeb@gmail.com>
2026-06-10 14:24:38 +0530

    Feat: Button tap

--- ios application/ios application/ContentView.swift
+++ ios application/ios application/ContentView.swift
@@ -8,16 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var count = 0
+    
     var body: some View {
+        
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+        
+            Text("Score : \(count)")
+                .font(.system(size:30))
+            
+            Spacer()
+            
+            Button {
+                count += 1
+            } label: {
+                Text("Tap me!")
+                    .frame(width: 200, height: 200)
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(Color.white)
+                    .font(.system(size:30))
+                    .clipShape(Circle())
+            }
+            
+            Spacer()
+            
+            Text("Timer")
+                .font(.system(size:30))
+            
+            
+            
+                }
+            }
         }
-        .padding()
-    }
-}
+        
 
 #Preview {
     ContentView()
