fbcc240b7e3447934844ee84fb6c50da1f51eb9c
adheeb-ahamed <official.adheeb@gmail.com>
2026-06-10 15:20:27 +0530

    Create a timer and once the timer stops the button stops adding score

--- ios application/ios application/ContentView.swift
+++ ios application/ios application/ContentView.swift
@@ -6,23 +6,34 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
     @State private var count = 0
+    @State private var timerLeft = 10
+    @State private var isTimerRunning = false
+    
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     var body: some View {
         
         
         VStack {
             
-        
+            
             Text("Score : \(count)")
                 .font(.system(size:30))
             
             Spacer()
             
             Button {
-                count += 1
+                if timerLeft <= 10 && timerLeft > 0{
+                    count += 1
+                }
+                
+                if timerLeft == 10 {
+                    isTimerRunning = true
+                }
             } label: {
                 Text("Tap me!")
                     .frame(width: 200, height: 200)
@@ -31,20 +42,29 @@ struct ContentView: View {
                     .foregroundColor(Color.white)
                     .font(.system(size:30))
                     .clipShape(Circle())
+                
             }
             
             Spacer()
             
-            Text("Timer")
+            Text("Timer: \(timerLeft)")
                 .font(.system(size:30))
             
             
             
-                }
+            
+        }
+        .onReceive(timer) { _ in
+            if isTimerRunning && timerLeft > 0 {
+                timerLeft -= 1
+            } else if timerLeft == 0 {
+                isTimerRunning = false
+                
             }
         }
-        
-
+    }
+}
 #Preview {
     ContentView()
 }
+
