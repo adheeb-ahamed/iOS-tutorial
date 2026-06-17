69a946022f8b86feb0c80293e4d932d4a23655df
adheeb-ahamed <official.adheeb@gmail.com>
2026-06-17 18:42:55 +0530

    Create timer score bar on Light It up game

---
+++ ios application/ios application/BlinkGame.swift
@@ -0,0 +1,176 @@
+//
+//  BlinkGame.swift
+//  ios application
+//
+//  Created by Student 3 on 2026-06-17.
+//
+
+import SwiftUI
+import Combine
+
+struct BlinkGame: View {
+    
+    @State private var scoreResult: Int = 0
+    
+    @State private var level : Int = 1
+    
+    //To create a timer
+    @State private var timerLeft = 60                            //CHANGE THIS TESTING PURPOSE
+    @State private var isTimerRunning = true
+    
+    //Dynamic array of cards
+    @State private var cards: [Card] = [
+        Card(isLit: false),
+        Card(isLit: false),
+        Card(isLit: false)
+    ]
+    
+    //Identifiable cards each unique
+    struct Card : Identifiable {
+        let id = UUID()
+        var isLit: Bool
+    }
+    
+    //create grid item
+    let columns = [
+        GridItem(),
+        GridItem(),
+        GridItem()
+    ]
+    
+    //to get how much time is spent
+    var elapsedTime: Int  {
+        60 - timerLeft
+    }
+
+    
+    //Create an internal timer
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    
+    var body: some View {
+        
+        ZStack{
+            Rectangle()
+                .fill(Color.white)
+                .frame(width: 410, height: 370)
+                .shadow(color: .black, radius: 0.5)
+                .position(x: 200, y: -100)
+            
+            
+            //Title
+            Text("Light it up!")
+                .font(.largeTitle)
+                .foregroundColor(Color.blue)
+                .padding()
+                .bold(true)
+                .position(x: 110, y: 35)
+            
+            Text("Score")
+                .font(.system(size: 20))
+                .bold(true)
+                .position(x: 55, y: 110)
+            
+            
+            Text ("\(scoreResult)")
+                .font(.system(size: 40))
+                .bold(true)
+                .foregroundStyle(Color.blue)
+                .position(x: 55, y: 150)
+            
+            Text("Level: ")
+                .font(.system(size: 20))
+                .bold(true)
+                .position(x: 300, y: 130)
+            
+            Text ("\(level)/4")
+                .font(.system(size: 40))
+                .bold(true)
+                .foregroundStyle(Color.blue)
+                .position(x: 360, y: 130)
+            
+            
+            VStack{
+                LazyVGrid(columns : columns){
+                    ForEach(cards) { card in
+                        Button(action: {
+                            
+                        }) {
+                            Rectangle()
+                                .fill(card.isLit ? Color.yellow : Color.blue)
+                                .frame (width: 115, height: 180)
+                                .cornerRadius(12)
+                                
+                        }
+                    }
+                }
+            }
+            .position(x: 200, y: 400)
+            
+            
+            Text("Timer: \(timerLeft)")
+                .font(.system(size: 20))
+                .bold(true)
+                .foregroundStyle(Color.blue)
+                .position(x: 200, y: 730)
+            
+            
+        }// End of Zstack
+        //this modifier explains the each passing second of timer
+        .onReceive(timer) { _ in
+            if isTimerRunning == true && timerLeft > 0 {
+                timerLeft -= 1
+                
+                //Level checker
+                if elapsedTime < 15 {
+                    level = 1
+                    cards.randomElement()?.isLit = true
+                }
+                else if elapsedTime < 30 {
+                    level = 2
+                    cards.randomElement()?.isLit = true
+                }
+                else if elapsedTime < 45 {
+                    level = 3
+                    cards.randomElement()?.isLit = true
+                }
+                else{
+                    level = 4
+                    cards.randomElement()?.isLit = true
+                }
+                
+                setupCards(for: level)
+                
+            }else if timerLeft == 0{
+                isTimerRunning = false
+
+            }
+        }
+        
+    }
+    
+    func setupCards(for newLevel: Int){
+        switch newLevel {
+        case 1:
+            cards = Array(repeating: Card (isLit: false), count: 3)
+            
+        case 2:
+            cards = Array(repeating: Card (isLit: false), count: 4)
+            
+        case 3:
+            cards = Array(repeating: Card (isLit: false), count: 6)
+            
+        case 4:
+            cards = Array(repeating: Card (isLit: false), count: 9)
+            
+        default :
+            break
+        }
+        
+        
+    }
+}
+#Preview {
+    BlinkGame()
+}
+
--- ios application/ios application/MainView.swift
+++ ios application/ios application/MainView.swift
@@ -14,135 +14,142 @@ struct MainView: View {
     //To go to Light it up game
     @State private var startLightItUpGame = false
     
+    
+    
     var body: some View {
     NavigationStack {
-        ScrollView{
-                ZStack{
-                    
-                    //Top rectangle with the title
-                    Rectangle()
-                        .fill(Color.white)
-                        .frame(width: 410, height: 150)
-                        .shadow(color: .black, radius: 0.5)
-                        .position(x: 200, y: 0)
-                    
-                    //Title
-                    Text("Velocity")
-                        .font(.largeTitle)
-                        .foregroundColor(Color.blue)
-                        .padding()
-                        .bold(true)
-                        .position(x: 200, y: 30)
-                    
-                    //Ready to play
-                    Text("Ready to play")
-                        .font(.system(size: 29, weight: .semibold, design: .rounded))
-                        .position(x: 120, y: 120)
-                    
+        ZStack{
             
-                    Text ("Simple light minded games")
-                        .font(.system(size: 20, weight: .light, design: .rounded))
-                        .position(x: 145, y: 155)
-                    
-                    //Image with rounded rectangle
-                    Image("tapImage")
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 350, height: 362)
-                        .clipShape(RoundedRectangle(cornerRadius: 15))
-                        .shadow(color: .black, radius: 0.5)
-                        .position(x: 200, y: 380)
-                    
-                    Rectangle()
-                        .fill(Color.white)
-                        .frame(width: 300, height: 250)
-                        .cornerRadius(15)
-                        .shadow(color: .black, radius: 0.5)
-                        .position(x: 200, y: 410)
-                    
-                    
-                    Text("Tap \nFrenzy")
-                        .font(.largeTitle)
-                        .padding()
-                        .bold(true)
-                        .position(x: 130, y: 350)
-                    
-                    Text ("Tap the button and score")
-                        .position(x: 164, y: 395)
-                        .padding()
-                        .font(.system(size: 20, weight: .light, design: .rounded))
-                    
-                    Button(action: {
-                        startTapGame = true
-                    }, label: {
-                        Text("Play")
-                            .frame(width: 250, height: 50)
-                            .background(Color.blue)
-                            .foregroundStyle(Color.white)
-                            .cornerRadius(15)
-                            .font(.system(size: 20, weight: .semibold, design: .rounded))
-                    })
-                    .position(x: 200, y: 480)
-                    
-                    
-                    //2ND GAME
-                    //Image with rounded rectangle
-                    Image("lightImage")
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 350, height: 362)
-                        .clipShape(RoundedRectangle(cornerRadius: 15))
-                        .shadow(color: .black, radius: 0.5)
-                        .position(x: 200, y: 780)
-                    
-                    Rectangle()
-                        .fill(Color.white)
-                        .frame(width: 300, height: 250)
-                        .cornerRadius(15)
-                        .shadow(color: .black, radius: 0.5)
-                        .position(x: 200, y: 810)
-                    
-                    
-                    Text("Light it \nup")
-                        .font(.largeTitle)
-                        .padding()
-                        .bold(true)
-                        .position(x: 130, y: 750)
-                    
-                    Text ("Tap the button and score")
-                        .position(x: 164, y: 795)
-                        .padding()
-                        .font(.system(size: 20, weight: .light, design: .rounded))
-                    
-                    Button(action: {
-                        startLightItUpGame = true
-                    }, label: {
-                        Text("Play")
-                            .frame(width: 250, height: 50)
-                            .background(Color.blue)
-                            .foregroundStyle(Color.white)
-                            .cornerRadius(15)
-                            .font(.system(size: 20, weight: .semibold, design: .rounded))
-                    })
-                    .position(x: 200, y: 880)
+            ScrollView{
+                    ZStack{
                         
-                    
-                    
-                    
-                    
-      
+                        //Ready to play
+                        Text("Ready to play")
+                            .font(.system(size: 29, weight: .semibold, design: .rounded))
+                            .position(x: 120, y: 120)
                         
-                    
-                    
-                        VStack(spacing : 20){
+                
+                        Text ("Simple light minded games")
+                            .font(.system(size: 20, weight: .light, design: .rounded))
+                            .position(x: 145, y: 155)
+                        
+                        //Image with rounded rectangle
+                        Image("tapImage")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 350, height: 362)
+                            .clipShape(RoundedRectangle(cornerRadius: 15))
+                            .shadow(color: .black, radius: 0.5)
+                            .position(x: 200, y: 380)
+                        
+                        Rectangle()
+                            .fill(Color.white)
+                            .frame(width: 300, height: 250)
+                            .cornerRadius(15)
+                            .shadow(color: .black, radius: 0.5)
+                            .position(x: 200, y: 410)
+                        
+                        
+                        Text("Tap \nFrenzy")
+                            .font(.largeTitle)
+                            .padding()
+                            .bold(true)
+                            .position(x: 130, y: 350)
+                        
+                        Text ("Tap the button and score")
+                            .position(x: 164, y: 395)
+                            .padding()
+                            .font(.system(size: 20, weight: .light, design: .rounded))
+                        
+                        Button(action: {
+                            startTapGame = true
+                        }, label: {
+                            Text("Play")
+                                .frame(width: 250, height: 50)
+                                .background(Color.blue)
+                                .foregroundStyle(Color.white)
+                                .cornerRadius(15)
+                                .font(.system(size: 20, weight: .semibold, design: .rounded))
+                        })
+                        .position(x: 200, y: 480)
+                        
+                        
+                        //2ND GAME
+                        //Image with rounded rectangle
+                        Image("lightImage")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 350, height: 362)
+                            .clipShape(RoundedRectangle(cornerRadius: 15))
+                            .shadow(color: .black, radius: 0.5)
+                            .position(x: 200, y: 780)
                             
                         
-                        } //End of VStack
+                        Rectangle()
+                            .fill(Color.white)
+                            .frame(width: 300, height: 250)
+                            .cornerRadius(15)
+                            .shadow(color: .black, radius: 0.5)
+                            .position(x: 200, y: 810)
                         
-                    } //End of ZStack
-                    .navigationDestination(isPresented: $startTapGame) {
-                        ContentView()
-                }
+                        
+                        Text("Light it \nup")
+                            .font(.largeTitle)
+                            .padding()
+                            .bold(true)
+                            .position(x: 130, y: 750)
+                        
+                        Text ("Light the button before it dims")
+                            .position(x: 183, y: 795)
+                            .padding()
+                            .font(.system(size: 20, weight: .light, design: .rounded))
+                        
+                        Button(action: {
+                            startLightItUpGame = true
+                        }, label: {
+                            Text("Play")
+                                .frame(width: 250, height: 50)
+                                .background(Color.blue)
+                                .foregroundStyle(Color.white)
+                                .cornerRadius(15)
+                                .font(.system(size: 20, weight: .semibold, design: .rounded))
+                        })
+                        .position(x: 200, y: 880)
+                            
+          
+                            Spacer()
+                        
+                        
+                            VStack(spacing : 20){
+                                
+                            
+                            } //End of VStack
+                            
+                        } //End of ZStack
+                        .navigationDestination(isPresented: $startTapGame) {
+                            ContentView()
+                        }
+                        .navigationDestination(isPresented: $startLightItUpGame){
+                            BlinkGame()
+                        }
+                        .padding(.bottom, 600)
+                
+            }//scrollable view
+            
+            //Top rectangle with the title
+            Rectangle()
+                .fill(Color.white)
+                .frame(width: 410, height: 150)
+                .shadow(color: .black, radius: 0.5)
+                .position(x: 200, y: 0)
+            
+            //Title
+            Text("Velocity")
+                .font(.largeTitle)
+                .foregroundColor(Color.blue)
+                .padding()
+                .bold(true)
+                .position(x: 200, y: 30)
         }
         
         
