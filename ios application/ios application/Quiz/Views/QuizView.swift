5bf02e685800879dd7003bf918b266f33940b6bf
adheeb-ahamed <official.adheeb@gmail.com>
2026-06-30 16:41:05 +0530

    Create UI design to the quiz

--- ios application/ios application/Quiz/Views/QuizView.swift
+++ ios application/ios application/Quiz/Views/QuizView.swift
@@ -12,6 +12,11 @@ struct QuizView: View {
     
     @Binding var showGame: Bool
     
+    let columns = [
+        GridItem (.flexible()),
+        GridItem (.flexible())
+    ]
+    
     var body: some View {
         Group {
             switch vm.viewState {
@@ -59,24 +64,62 @@ struct QuizView: View {
     //  This is where quizView takes place
     //---------------------------------------------
     var quizView : some View {
-        VStack (spacing : 20) {
+        VStack (spacing : 40) {
+            HStack{
+                Text ("Score : \(vm.score)")
+                    .font(.headline)
+                
+                Spacer()
+                
+                Text ("Question: \(vm.currentIndex + 1) of \(vm.questions.count)")
+                    .font(.headline)
+            }
+            .padding(.horizontal)
+            
+            Spacer()
+            
             
             Text (vm.questions[vm.currentIndex].question)
+                .font(.title2)
+                .fontWeight(.bold)
+                .multilineTextAlignment(.center)
+                .padding()
+                .frame(maxWidth: .infinity)
+                .frame(height: 200) // Gives it that large box size
+                .background(Color(.systemGray5)) // Light gray background
+                .cornerRadius(12) // Rounded corners
+                .padding(.horizontal)
             
-            ForEach(answerOptions, id: \.self){ option in
-                Button(option){
-                    vm.answer(option)
+            
+            LazyVGrid(columns: columns, spacing : 16){
+                ForEach(answerOptions, id: \.self){ option in
+                    Button(action: {
+                        vm.answer(option)
+                    }){
+                        Text (option)
+                            .font(.title2)
+                            .bold()
+                            .foregroundColor(.black)
+                            .frame(maxWidth: .infinity) // Makes button fill column width
+                            .frame(height: 80)
+                            .padding(.vertical, 20)      // Gives it that tall, thick look
+                            .background(Color(.systemGray4)) // Light gray background
+                            .cornerRadius(12)
+                    }
                 }
-                
             }
-            Text("Score: \(vm.score)")
+            .padding(.horizontal)
+            
+            Spacer()
+            
+            
         }
     }
     
     var answerOptions : [String] {
         let current = vm.questions[vm.currentIndex]
         
-        return (current.incorrect_answer + [current.correct_answer]).shuffled()
+        return (current.incorrect_answers + [current.correct_answer]).shuffled()
         
     }
 
