fade5d6c5be61ea6bda180b801b20de72f35e090
adheeb-ahamed <official.adheeb@gmail.com>
2026-06-30 11:47:04 +0530

    Add codable models for Quiz game

---
+++ ios application/ios application/Quiz/Data model/QuizResponse.swift
@@ -0,0 +1,19 @@
+//
+//  QuizResponse.swift
+//  ios application
+//
+//  Created by Student 3 on 2026-06-30.
+//
+
+import Foundation
+
+struct QuizResponse: Codable {
+    let results: [Question]
+}
+
+
+struct Question: Codable {
+    let question : String
+    let correct_answer : String
+    let incorrect_answer : String
+}
---
+++ ios application/ios application/quiz.swift
@@ -0,0 +1,21 @@
+//
+//  quiz.swift
+//  ios application
+//
+//  Created by Student 3 on 2026-06-30.
+//
+
+import SwiftUI
+
+struct quiz: View {
+    var body: some View {
+        
+        Text("Hello world")
+        
+        
+        
+    }//End of body
+}//End of view
+#Preview {
+    quiz()
+}
